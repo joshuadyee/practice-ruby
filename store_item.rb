e31274dd70d1efa9cc17a8645d665ab283d0b78c
Joshua Yee <joshyee16@gmail.com>
2023-11-28 12:08:11 -0800

    change initialize to one parameter, change the new instance args to hashes

--- store_item.rb
+++ store_item.rb
@@ -11,26 +11,22 @@ p paper
 class Item
   attr_reader :color, :price, :count
   attr_writer :color, :price, :count
-  def initialize(color, price, count)
-    @color = color
-    @price = price
-    @count = count
+  def initialize(options)
+    @color = options[:color]
+    @price = options[:price]
+    @count = options[:count]
   end
 end
 
-erasers = Item.new("pink", 3, 5)
-p erasers
-p erasers.color
-
-pencils = Item.new("yellow", 5, 12)
+pencils = Item.new(color: "yellow", price: 5, count: 12)
 p pencils
 p pencils.price
 
-pens = Item.new("blue", 7, 15)
+pens = Item.new(color: "blue", price: 7, count: 15)
 p pens
 p pens.count
 
-paper = Item.new("white", 12, 500)
+paper = Item.new(color: "white", price: 12, count: 500)
 p paper
 
 p paper.color
