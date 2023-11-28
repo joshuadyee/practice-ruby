c4546343e62136c5d72b7d2b8bb17ceb27ea7a23
Joshua Yee <joshyee16@gmail.com>
2023-11-28 11:35:16 -0800

    write print statements using reader attributes

--- store_item.rb
+++ store_item.rb
@@ -20,12 +20,15 @@ end
 
 erasers = Item.new("pink", 3, 5)
 p erasers
+p erasers.color
 
 pencils = Item.new("yellow", 5, 12)
 p pencils
+p pencils.price
 
 pens = Item.new("blue", 7, 15)
 p pens
+p pens.count
 
 paper = Item.new("white", 12, 500)
 p paper
